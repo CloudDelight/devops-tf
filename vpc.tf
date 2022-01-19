e053d4ef3829bd90cbbb7bafd3295ef616309bb2
CloudDelight <root@ip-172-31-18-38.ec2.internal>
2022-01-19 11:20:30 +0000

    Adding vpc

---
+++ vpc.tf
@@ -0,0 +1,50 @@
+resource "aws_vpc" "demo_vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "demo-vpc"
+  }
+}
+
+resource "aws_subnet" "demo_vpc_public_subnet_1" {
+  vpc_id     = aws_vpc.demo_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "public-subnet-1"
+  }
+}
+
+
+resource "aws_subnet" "demo_vpc_public_subnet_2" {
+  vpc_id     = aws_vpc.demo_vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "public-subnet-2"
+  }
+}
+
+resource "aws_subnet" "demo_vpc_private_subnet_1" {
+  vpc_id     = aws_vpc.demo_vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "private-subnet-1"
+  }
+}
+
+resource "aws_subnet" "demo_vpc_private_subnet_2" {
+  vpc_id     = aws_vpc.demo_vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "private-subnet-2"
+  }
+}
+
+resource "aws_internet_gateway" "demo_igw" {
+  vpc_id = aws_vpc.demo_vpc.id
+}
+
